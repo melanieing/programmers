9d98390a1949b8f02008d56fc9de5884c6111798
Melanie Woo <91274798+melanieing@users.noreply.github.com>
2022-11-28 20:55:15 +0900

    120804. 두 수의 곱

---
+++ level0/Multiple120804.java
@@ -0,0 +1,10 @@
+class Solution {
+  // 코딩테스트 입문 : 두 수의 곱
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        answer = num1 * num2;
+        
+        return answer;
+    }
+}
