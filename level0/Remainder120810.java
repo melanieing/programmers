58c1dbe3ecefeda8554884918b42f9cce10ee296
Melanie Woo <91274798+melanieing@users.noreply.github.com>
2022-11-28 20:54:04 +0900

    120810. 나머지 구하기

---
+++ level0/Remainder120810.java
@@ -0,0 +1,10 @@
+class Solution {
+  // 코딩 테스트 입문 : 나머지 구하
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = num1 % num2;
+        
+        return answer;
+    }
+}
