9b207ee441ee9ff57db657dc8876a8c38ac7dbbb
Melanie Woo <91274798+melanieing@users.noreply.github.com>
2022-11-28 20:52:35 +0900

    120807. 숫자 비교하기

---
+++ level0/CompareNums120807.java
@@ -0,0 +1,13 @@
+class Solution {
+  // 코딩테스트 입문 : 숫자 비교하기
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if (num1 == num2) {
+            answer = 1;
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
