c0c4eed4d50983582b43096b373bce9d6d30f1a0
Melanie Woo <91274798+melanieing@users.noreply.github.com>
2023-05-01 23:38:39 +0900

    Create TargetNm43165.java

---
+++ level2/TargetNm43165.java
@@ -0,0 +1,26 @@
+class Solution {
+  
+    // 이진트리를 그려서 좌측 간선은 해당 노드값을 더하는 경우, 우측 간선은 해당 노드값을 빼는 경우로 생각해보기
+    // 좌측부터 깊이 우선 탐색을 진행하기
+  
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+                
+        dfs(numbers, 0, target, 0); 
+        
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int level, int target, int sum) {
+        if (level == numbers.length) { // 끝까지 탐색한 경우
+            if (target == sum) { // 타겟 넘버와 일치하면 개수 올리기
+                answer++;
+            }
+        } else { // 탐색할 노드가 남아있는 경우
+            dfs(numbers, level+1, target, sum + numbers[level]); // 해당 노드 값 더하기
+            dfs(numbers, level+1, target, sum - numbers[level]); // 해당 노드 값 빼기
+        }
+    }
+    
+}
