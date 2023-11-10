bfa4dca2e66ae8deb4b95d799d213774f33ec2ad
melanieing <melanie0617@gmail.com>
2023-11-10 13:52:41 +0900

    87390. n^2 배열 자르기

--- level2/NthPower87390.java
+++ level2/NthPower87390.java
@@ -1,45 +1,45 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 87390. n^2 배열 자르기
+ * 주어진 과정대로 만들어진 1차원 배열을 return
+ */
 public class NthPower87390 {
     public static void main(String[] args) {
         NthPower87390 q = new NthPower87390();
-        int n = 3;
-        int left = 2;
-        int right = 5;
+        int n = 4;
+        int left = 7;   // 7 -> 2(left/n+1)행 3(left % n)인덱스
+        int right = 14; // 14 -> 4(right/n+1)행 2(right % n)인덱스
 
         System.out.println(Arrays.toString(q.solution(n, left, right)));
     }
 
-    public int[] solution(int n, int left, int right) {
-        int[] answer = new int[n*n];
-        int idx = 0;
+    public int[] solution(int n, long left, long right) {
         ArrayList<Integer> list = new ArrayList<>();
-        int[][] arr = new int[n][n];
-
-        int tmp = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i+1; j++) {
-                arr[i][j] = tmp;
-                System.out.println("i = " + i + " / j : " + j);
-                if (i == j) {
-                    tmp++;
-                } else if (i <= j)
-                answer[idx] = arr[i][j];
-                if (idx >= left && idx <= right) {
-                    list.add(answer[idx]);
-                }
+        for (long i = left / n+ 1; i <= right / n + 1; i++) { // left가 있는 행부터 right 가 있는 행까지만 돌기
+            long cnt = i;
+            int idx = 0;
+            while (cnt-- > 0) {
+                list.add((int)i);
                 idx++;
             }
-        }
-
-        for (int[] a : arr) {
-            for (int b : a) {
-                System.out.print(b + " ");
+            long tmp = i+1;
+            int rest = n - idx;
+            while (rest-- > 0) {
+                list.add((int)tmp++);
             }
-            System.out.println();
         }
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[(int)(right-left+1)];
+        int idx = 0;
+        long start = left % n; // left 인덱스부터 시작
+
+        while (idx != answer.length) {
+            answer[idx++] = list.get((int)start++);
+        }
+
+        return answer;
     }
 }
+
